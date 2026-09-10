96055f92bedc28451e4bf9565db539b5c5c595f2
khantpyaehtoo <ericrebillet@gmail.com>
2026-09-10 14:09:06 +0630

    update:

--- types/user.ts
+++ types/user.ts
@@ -1,7 +1,7 @@
-import { IconName } from "@/components/ui/icon";
+import { ReactNode } from "react";
 
 export interface NavItem {
     label: string;
     href: string;
-    icon: IconName;
+    icon: ReactNode;
 }
